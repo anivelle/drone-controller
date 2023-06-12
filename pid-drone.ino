3ed8b359d4c6f0dfa95ee3b6801c82509abb9d4f
Alex Nivelle <anivelle@ucsc.edu>
2023-06-12 01:13:41 -0700

    Realized I can just use one PWM module, but I do have to interweave the compare values into one sequence to be loaded from RAM, it's a little gross

--- pid-drone.ino
+++ pid-drone.ino
@@ -4,7 +4,8 @@
 
 #define SEQ_SIZE 17
 #define TELEMETRY 0
-uint16_t command;
+
+uint16_t seq[SEQ_SIZE * 4];
 
 /**
  * Takes a command (currently in the form 0000TSSSSSSSSSSS), and calculates a
@@ -16,22 +17,66 @@ void CalcChecksum(uint16_t *command) {
     *command = (*command << 4) | temp;
 }
 
-void CreateSequence(uint16_t command, uint16_t sequence[17]) {
-    command = (command << 1) | TELEMETRY;
-    CalcChecksum(&command);
-    int j;
-    // Send LSB last
-    for (int i = 15; i > 0; i--) {
-        sequence[i] = (5 * (1 << ((command >> i) & 1))) | (1 << 15);
+void CreateSequence(uint16_t command[4], uint16_t sequence[17]) {
+    uint16_t temp[4];
+    for (int i = 0; i < 4; i++) {
+        temp[i] = command[i];
+        temp[i] = (temp[i] << 1) | TELEMETRY;
+        CalcChecksum(&temp[i]);
     }
-    sequence[16] = 1 << 15;
+    int j;
+    /* Send LSB last
+     * 0 is a duty cycle of 5, 1 is a duty cycle of 10, so I simply multiply
+     * 5 by 1 or 2, depending on whether the ith bit of command is 1.
+     * 1 shifted over 15 indicates the first edge to be falling, so the PWM
+     * output starts high. This operates on all four DShot commands, so it kinda
+     * takes a bit of time. Good things microcontrollers are fast and these are
+     * technically quick commands.
+     */
+    for (int i = 15; i > 0; i--) {
+        for (j = 0; j < 4; j++) {
+            sequence[(i << 2) + j] =
+                (5 * (1 << ((temp[j] >> i) & 1))) | (1 << 15);
+        }
+    }
+    // The last compare of the PWM sequences are always 0, so the last four
+    // values of the sequence are 0
+    for (int i = 0; i < 4; i++) {
+        sequence[SEQ_SIZE * 4 - i - 1] = 1 << 15;
+    }
 }
 
+void PWM_Init() {
+    NRF_PWM0->ENABLE = 1;
+
+    // I think this may be okay just being set once
+    NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
+}
+
+/**
+ * Connects a digital pin to the PWM module
+ * Important note: PWM cannot be enabled before adding a pin. Check the
+ * Nano 33 BLE pinout for info on port and pin values
+ */
+int PWM_AddPins(uint8_t channel, uint8_t port, uint32_t pin) {
+    // PWM cannot be enabled
+    if (NRF_PWM0->ENABLE == 1)
+        return -1;
+    // Only 4 channels
+    if (channel > 3)
+        return -2;
+    if (pin > 31 || port > 1)
+        return -3;
+
+    // Last 5 bits indicate pin number, bit 6 is port
+    NRF_PWM0->PSEL.OUT[channel] = pin | port << 5;
+}
+
+void SendCommand(uint8_t port, uint16_t *seq) {}
+
 void setup() {
-    Serial.begin(9600);
-    uint16_t seq[SEQ_SIZE];
     uint16_t command = 1046;
-    CreateSequence(command, seq);
+    CreateSequence(command, seq[0]);
 
     NRF_TIMER2->INTENSET = 0x000F0000;
     pinMode(P1_11, OUTPUT);
@@ -44,7 +89,7 @@ void setup() {
     NRF_PWM0->PRESCALER = PWM_PRESCALER_PRESCALER_DIV_1;
     NRF_PWM0->DECODER = PWM_DECODER_LOAD_Common |
                         (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);
-    NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
+    NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq[0];
     NRF_PWM0->SEQ[0].CNT = SEQ_SIZE;
     NRF_PWM0->SEQ[0].REFRESH = 0;
     NRF_PWM0->SEQ[0].ENDDELAY = 0;
