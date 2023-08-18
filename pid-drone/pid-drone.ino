70d2560576a4552adbf0b4ffe999fc7256b6191a
Alex Nivelle <anivelle@ucsc.edu>
2023-08-18 00:30:03 -0700

    Well I forgot that the nRF52840 repeats its last duty cycle, so I thought maybe that would fix my motor issues, but no

--- pid-drone/pid-drone.ino
+++ pid-drone/pid-drone.ino
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include "mbed.h"
 
-#define SEQ_SIZE 16
+#define SEQ_SIZE 17
 #define TELEMETRY 0
 #define POTENTIOMETER A0
 
@@ -31,6 +31,7 @@ void CreateSequence(uint16_t command, uint16_t sequence[17]) {
     for (int i = 0; i < 16; i++) {
         sequence[i] = (5 * (1 << ((command >> (15 - i)) & 1))) | (1 << 15);
     }
+    sequence[16] = 0;
 }
 
 void setup() {
@@ -41,7 +42,7 @@ void setup() {
     pinMode(P1_11, OUTPUT);
     pinMode(A0, INPUT);
     digitalWrite(P1_11, LOW);
-    NRF_PWM0->PSEL.OUT[0] = 11 | (1 << 5);
+    NRF_PWM0->PSEL.OUT[0] = P1_11;
     NRF_PWM0->ENABLE = 1;
     NRF_PWM0->MODE = PWM_MODE_UPDOWN_Up;
     NRF_PWM0->COUNTERTOP = 13;
@@ -59,24 +60,16 @@ void setup() {
 
 void loop() {
     curTime = millis();
-    // I want to try beeps to see if the commands are even sending correctly
-    for (int i = 1; i < 6; i++) {
-        command = i;
-        CreateSequence(command, seq);
-        NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
-        NRF_PWM0->TASKS_SEQSTART[0] = 1;
-        delay(300);
-    }
     if (curTime - prevTime > 100) {
         command = analogRead(A0) * 2;
         if (command < 48)
             command = 48;
-        Serial.println(command);
+        // Serial.println(command);
         CreateSequence(command, seq);
-        for (int i = 0; i < 16; i++) {
-            Serial.print('\t');
-            Serial.println(seq[i], BIN);
-        }
+        // for (int i = 0; i < 16; i++) {
+        //     Serial.print('\t');
+        //     Serial.println(seq[i], BIN);
+        // }
         NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
         NRF_PWM0->TASKS_SEQSTART[0] = 1;
         prevTime = curTime;
