1a3e45a3ca96caa680b93767083ca6521fd7739e
Alex Nivelle <anivelle@ucsc.edu>
2023-09-03 23:15:43 -0700

    The BNO055 library should be right, but nothing works!!! I realized I was using the wrong pins on the Arduino, but that didn't fix it

--- I2CTesting/I2CTesting.ino
+++ I2CTesting/I2CTesting.ino
@@ -1,10 +1,42 @@
-#include "CustomI2C.h"
 #include <stdint.h>
-#include "mbed.h"
-#include "VL53L4CX.h"
+#include <Wire.h>
+#include "BNO055.h"
 
-VL53L4CX distanceSensor(&Wire, A1);
+BNO055 gyro(&Wire);
+int err;
 
-void setup() { Wire.begin(); }
+void setup() {
+    Serial.begin(115200);
+    // Very important for serial to work properly (inside setup, at least)
+    while (!Serial) {
+    }
+    Wire.begin();
+    // Configuration stuff here
+    delay(850);
+    err = gyro.writeRegister(OPR_MODE, AMG);
+    delay(30);
+    if (err != 0) {
+        Serial.println("ERROR: Did not write properly");
+        Serial.println(err);
+    }
+    err = gyro.readRegister(OPR_MODE);
+    if (err != AMG) {
+        Serial.println("ERROR: Operation mode not set");
+        Serial.println(err);
+    } else {
+        Serial.println("We're good");
+    }
+}
 
-void loop() {}
+void loop() {
+    while (true) {
+    }
+    uint8_t ready = gyro.readRegister(INT_STA);
+    uint16_t acc[3];
+    if (ready & 1) {
+        for (int i = 0; i < 3; i++) {
+            acc[i] = gyro.readAcc((Axis)i);
+            // Serial.println(acc[i]);
+        }
+    }
+}
