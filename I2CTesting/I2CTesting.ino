850514b664b6e10a4fba7ad79a0aba57599ca845
Alex Nivelle <anivelle@ucsc.edu>
2023-09-08 22:54:58 -0700

    Well I've got it working with mbed API calls, but I don't like this method. I may take a look at the underlying code to see where I'm going wrong

--- I2CTesting/I2CTesting.ino
+++ I2CTesting/I2CTesting.ino
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <Wire.h>
 #include "BNO055.h"
+#include "mbed.h"
 
 typedef enum State { CONFIGURING, CALIBRATING, FLYING } State_t;
 BNO055 gyro(&Wire);
@@ -13,19 +14,21 @@ State nextState;
 uint8_t makeTransition = 0;
 uint8_t calib_state = 0xFF;
 
+mbed::Ticker ticker;
+
 const char *titles[] = {"Heading", "Roll", "Pitch"};
 volatile uint8_t drdy;
 int angle[3];
 volatile int temp;
 long prevTime;
 
-void TIMER1_IRQHandler() {
-    if (NRF_TIMER1->EVENTS_COMPARE[0]) {
-        drdy = 1;
-        NRF_TIMER1->EVENTS_COMPARE[0] = 0;
-        temp = NRF_TIMER1->EVENTS_COMPARE[0];
-        temp;
-    }
+void timer_callback() {
+    // if (NRF_TIMER1->EVENTS_COMPARE[0]) {
+    drdy = 1;
+    //    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+    //    temp = NRF_TIMER1->EVENTS_COMPARE[0];
+    //    temp;
+    //}
 }
 
 void setup() {
@@ -39,9 +42,9 @@ void setup() {
     delay(850); // Startup time for I2C? Not sure if necessary
 
     // Timer interrupt for the IMU
-    NRF_TIMER1->PRESCALER = PWM_PRESCALER_PRESCALER_DIV_32;
-    NRF_TIMER1->CC[0] = 5000;
-    NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_16Bit;
+    // NRF_TIMER1->PRESCALER = PWM_PRESCALER_PRESCALER_DIV_32;
+    // NRF_TIMER1->CC[0] = 5000;
+    // NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_16Bit;
     // NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Msk;
     // NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;
     // NVIC_EnableIRQ(TIMER1_IRQn);
@@ -60,6 +63,8 @@ void setup() {
     } else {
         Serial.println("We're good");
     }
+
+    ticker.attach(&timer_callback, 0.012);
     curTime = millis();
     units = gyro.readRegister(UNIT_SEL);
     Serial.println("Beginning Calibration");
@@ -94,9 +99,9 @@ void loop() {
         }
         break;
     case FLYING:
-        if (millis() - prevTime > 10) {
-            NRF_TIMER1->TASKS_CAPTURE[1] = 1;
-            Serial.println(NRF_TIMER1->CC[1]);
+        // if (millis() - prevTime > 10) {
+        if (drdy) {
+            // Serial.println(NRF_TIMER1->CC[1]);
             for (int i = 0; i < 3; i++) {
                 angle[i] = gyro.readEul((Axis)i);
                 if (units & 4)
@@ -108,8 +113,8 @@ void loop() {
                 Serial.println(angle[i]);
             }
             drdy = 0;
-            NRF_TIMER1->TASKS_CLEAR = 1;
-            NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+            // NRF_TIMER1->TASKS_CLEAR = 1;
+            // NRF_TIMER1->EVENTS_COMPARE[0] = 0;
             prevTime = millis();
         }
         break;
