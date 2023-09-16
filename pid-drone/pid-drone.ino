ac77533db5c8d88ee6c7e4cab120ef6f2efae047
Alex Nivelle <anivelle@ucsc.edu>
2023-09-16 13:33:21 -0700

    Fixed DShot but the ESC has pull-ups on the motor wires and also the Arduino does not respond to resets

--- pid-drone/pid-drone.ino
+++ pid-drone/pid-drone.ino
@@ -2,18 +2,17 @@
 #include <stdint.h>
 #include "mbed.h"
 
-#define ONESHOT
 #define SEQ_SIZE 17
 #define TELEMETRY 0
 #define POTENTIOMETER A0
 
 uint16_t command;
 uint16_t throttle;
-uint16_t oneshot[2];
 
 uint16_t seq[SEQ_SIZE];
 unsigned int prevTime;
 unsigned int curTime;
+uint8_t counter = 0;
 
 /**
  * Takes a command (currently in the form 0000TSSSSSSSSSSS), and calculates a
@@ -26,37 +25,31 @@ void CalcChecksum(uint16_t *command) {
 }
 
 void CreateSequence(uint16_t command, uint16_t sequence[17]) {
-    command = (command << 1) | TELEMETRY;
-    CalcChecksum(&command);
+    uint16_t comm = (command << 1) | TELEMETRY;
+    CalcChecksum(&comm);
     int j;
     // Send LSB last
     for (int i = 0; i < 16; i++) {
-        sequence[i] = (5 * (1 << ((command >> (15 - i)) & 1))) | (1 << 15);
+        sequence[i] = (5 * (1 << ((comm >> (15 - i)) & 1))) | (1 << 15);
     }
-    sequence[16] = 0;
+    sequence[16] = 0 | (1 << 15);
 }
 
 void setup() {
     Serial.begin(9600);
-    command = 48;
+    while (!Serial) {
+    }
+    command = 0;
     CreateSequence(command, seq);
-    oneshot[0] = 2000;
-    oneshot[1] = 0;
     pinMode(P1_11, OUTPUT);
     pinMode(A0, INPUT);
-    digitalWrite(P1_11, LOW);
+    // digitalWrite(P1_11, LOW);
     NRF_PWM0->PSEL.OUT[0] = P1_11;
     NRF_PWM0->ENABLE = 1;
     NRF_PWM0->MODE = PWM_MODE_UPDOWN_Up;
-#ifndef ONESHOT
     NRF_PWM0->COUNTERTOP = 13;
     NRF_PWM0->SEQ[0].CNT = SEQ_SIZE;
     NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
-#else
-    NRF_PWM0->COUNTERTOP = 5000;
-    NRF_PWM0->SEQ[0].CNT = 2;
-    NRF_PWM0->SEQ[0].PTR = (uint32_t)&oneshot;
-#endif
     NRF_PWM0->LOOP = 0;
     NRF_PWM0->PRESCALER = PWM_PRESCALER_PRESCALER_DIV_1;
     NRF_PWM0->DECODER = PWM_DECODER_LOAD_Common |
@@ -69,23 +62,23 @@ void setup() {
 
 void loop() {
     curTime = millis();
+    Serial.println("Hello");
+    command = 0;
     if (curTime - prevTime > 100) {
-        command = analogRead(A0) * 2;
-        if (command < 48)
-            command = 48;
-            // Serial.println(command);
-#ifndef ONESHOT
+        if (counter > 10) {
+            command = analogRead(A0) * 2;
+            if (command < 48)
+                command = 48;
+        }
+        // Serial.println(command);
         CreateSequence(command, seq);
         NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
-#else
-        oneshot[0] = command / 1023 * 2000 + 2000;
-        NRF_PWM0->SEQ[0].PTR = (uint32_t)&oneshot;
-#endif
         // for (int i = 0; i < 16; i++) {
         //     Serial.print('\t');
         //     Serial.println(seq[i], BIN);
         // }
         NRF_PWM0->TASKS_SEQSTART[0] = 1;
         prevTime = curTime;
+        counter++;
     }
 }
