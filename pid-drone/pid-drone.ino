8f7abe57bf6e0ba37fcf32a012c52487933bccdb
Alex Nivelle <anivelle@ucsc.edu>
2024-01-15 21:21:33 -0800

    DShot/PWM libraries have been fixed to actually work with every motor now

--- pid-drone/pid-drone.ino
+++ pid-drone/pid-drone.ino
@@ -4,7 +4,6 @@
 #include "DShot.h"
 #include "PWM.h"
 
-#define SEQ_SIZE 17
 #define TELEMETRY 0
 #define POTENTIOMETER A0
 
@@ -12,6 +11,7 @@ uint16_t commands[4];
 uint16_t throttle;
 
 uint16_t seq[SEQ_SIZE];
+uint16_t seqOld[SEQ_SIZE / 4];
 unsigned int prevTime;
 unsigned int curTime;
 uint8_t counter = 0;
@@ -20,27 +20,27 @@ uint8_t counter = 0;
  * Takes a command (currently in the form 0000TSSSSSSSSSSS), and calculates a
  * checksum based on the 12 set bits. Modifies the command in place.
  */
-// void CalcChecksum(uint16_t *command) {
-//     uint16_t temp = *command;
-//     temp = ((temp ^ (temp >> 4)) ^ (temp >> 8)) & 0x0F;
-//     *command = (*command << 4) | temp;
-// }
-//
-// void CreateSequence(uint16_t command, uint16_t sequence[17]) {
-//     uint16_t comm = (command << 1) | TELEMETRY;
-//     CalcChecksum(&comm);
-//     int j;
-//     // Send LSB last
-//     for (int i = 0; i < 16; i++) {
-//         sequence[i] = (5 * (1 << ((comm >> (15 - i)) & 1))) | (1 << 15);
-//     }
-//     sequence[16] = 0 | (1 << 15);
-// }
+void CalcChecksumOld(uint16_t *command) {
+    uint16_t temp = *command;
+    temp = ((temp ^ (temp >> 4)) ^ (temp >> 8)) & 0x0F;
+    *command = (*command << 4) | temp;
+}
+
+void CreateSequenceOld(uint16_t command, uint16_t sequence[17]) {
+    uint16_t comm = (command << 1) | TELEMETRY;
+    CalcChecksumOld(&comm);
+    int j;
+    // Send LSB last
+    for (int i = 0; i < 16; i++) {
+        sequence[i] = (5 * (1 << ((comm >> (15 - i)) & 1))) | (1 << 15);
+    }
+    sequence[16] = 0 | (1 << 15);
+}
 
 void setup() {
-    Serial.begin(9600);
-    while (!Serial) {
-    }
+    // Serial.begin(9600);
+    // while (!Serial) {
+    // }
     commands[0] = 0;
     commands[1] = 0;
     commands[2] = 0;
@@ -50,8 +50,8 @@ void setup() {
     PWM_AddPins(1, P1_15);
     PWM_AddPins(2, P1_13);
     PWM_AddPins(3, P1_14);
-    pinMode(A0, INPUT);
-    PWM_Init(SEQ_SIZE, seq);
+    pinMode(POTENTIOMETER, INPUT);
+    PWM_Init((uint32_t)&seq);
     // NRF_PWM0->PSEL.OUT[0] = P1_12;
     // NRF_PWM0->ENABLE = 1;
     // NRF_PWM0->MODE = PWM_MODE_UPDOWN_Up;
@@ -72,11 +72,10 @@ void setup() {
 
 void loop() {
     curTime = micros();
-    Serial.println("Hello");
+    // Serial.println("Hello");
     uint16_t command;
     if (counter > 10) {
-
-        command = analogRead(A0) * 2;
+        command = analogRead(POTENTIOMETER) * 2;
         if (command < 48)
             command = 48;
         for (int i = 0; i < 4; i++) {
@@ -87,14 +86,31 @@ void loop() {
     if (curTime - prevTime > 200) {
         // Serial.println(command);
         CreateSequence(commands, seq);
+        CreateSequenceOld(commands[1], seqOld);
+        // if (counter == 100) {
+        //     Serial.println(counter);
+        //     for (int i = 0; i < 17; i++) {
+        //         Serial.print(seqOld[i] & 0xFF);
+        //         Serial.print(" ");
+        //     }
+        //     Serial.println();
+        //     for (int j = 0; j < 4; j++) {
+        //         for (int i = 0; i < 17; i++) {
+        //             Serial.print(seq[(i << 2) + j] & 0xFF);
+        //             Serial.print(" ");
+        //         }
+        //         Serial.println();
+        //     }
+        // }
         // NRF_PWM0->SEQ[0].PTR = (uint32_t)&seq;
         // for (int i = 0; i < 16; i++) {
         //     Serial.print('\t');
         //     Serial.println(seq[i], BIN);
         // }
-        PWM_SendCommand();
+        PWM_SendCommand((uint32_t)&seq);
         // NRF_PWM0->TASKS_SEQSTART[0] = 1;
         prevTime = curTime;
-        counter++;
+        if (counter < 255)
+            counter++;
     }
 }
